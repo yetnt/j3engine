e332e9b62ee57e25a6db61b5d4aa77c156ec4681
Lehlogonolo Poole <hlonipoole692@gmail.com>
2025-10-01 14:08:44 +0200

    cube.

--- src/main/java/com/j3d/Executor.java
+++ src/main/java/com/j3d/Executor.java
@@ -33,14 +33,43 @@ public class Executor {
 
 //        renderer.axis(graphics2D, Main.camera);
 
-        test();
+//        test();
+        cube();
     }
 
     public void test() {
-        GPoint A = new GPoint(new Vector3(-1, -1, 0));
-        GPoint B = new GPoint(new Vector3(0, -1, -2));
-        GPoint C = new GPoint(new Vector3(-10, 0, -2));
+        GPoint A = new GPoint(new Vector3(10, 0, 0));
+        GPoint B = new GPoint(new Vector3(0, 10, 0));
+        GPoint C = new GPoint(new Vector3(0, 0, 10));
         GTri triangl = new GTri(Color.ORANGE, A, B, C);
         new Thing(renderer, null).addObjs(triangl, triangl.getLegA(), triangl.getLegB(), triangl.getLegC(), A, B, C);
+    }
+
+    public Thing cube() {
+        GPoint A = new GPoint(new Vector3(-5, 5, -5));
+        GPoint B = new GPoint(new Vector3(-5, -5, -5));
+        GPoint C = new GPoint(new Vector3(5, 5, -5));
+        GTri face1tri1 = new GTri(Color.ORANGE, A, B, C);
+        GPoint D = new GPoint(new Vector3(5, -5, -5));
+        GTri face1tri2 = new GTri(Color.ORANGE, B, C, D);
+        GPoint E = new GPoint(new Vector3(5, 5, 5));
+        GTri face2tri1 = new GTri(Color.PINK, D, C, E);
+        GPoint F = new GPoint(new Vector3(5, -5, 5));
+        GTri face2tri2 = new GTri(Color.PINK, D, E, F);
+        GPoint H = new GPoint(new Vector3(-5, 5, 5));
+        GTri face3tri1 = new GTri(Color.GREEN, F, E, H);
+        GPoint G = new GPoint(new Vector3(-5, -5, 5));
+        GTri face3tri2 = new GTri(Color.GREEN, F, H, G);
+        GTri face4tri1 = new GTri(Color.BLUE, H, A, G);
+        GTri face4tri2 = new GTri(Color.BLUE, A, G, B);
+        GTri face5tri1 = new GTri(Color.LIGHT_GRAY, A, H, C);
+        GTri face5tri2 = new GTri(Color.LIGHT_GRAY, H, C, E);
+        GTri face6tri1 = new GTri(Color.YELLOW, D, F, B);
+        GTri face6tri2 = new GTri(Color.YELLOW, F, B, G);
+
+        return new Thing(renderer, null).addObjs(
+                A, B, C, D, E, F, G, face1tri1, face1tri2, face2tri1, face2tri2, face3tri1, face3tri2, face4tri1, face4tri2,
+                face5tri1, face5tri2, face6tri1, face6tri2
+        );
     }
 }
