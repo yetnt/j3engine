17c0390beba8155f4e3e662244fc11636d7adf34
Lehlogonolo Poole <hlonipoole692@gmail.com>
2025-10-05 08:39:05 +0200

    A subcommand is also an argument.

--- src/main/java/com/j3d/engine/interact/cmd/base/Subcommand.java
+++ src/main/java/com/j3d/engine/interact/cmd/base/Subcommand.java
@@ -5,7 +5,7 @@ package com.j3d.engine.interact.cmd.base;
  * It inherits from Command and does not add any new functionality.
  * It is used to differentiate between commands and subcommands.
  */
-public class Subcommand extends Command {
+public class Subcommand extends Command implements Argument {
     public Subcommand(String name, String description) {
         super(name, description);
     }
